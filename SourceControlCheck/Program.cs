85de0c527564bfe8e5cae4e66a583f1cc8249acb
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-07-06 14:30:49 -0700

    Changed the structure of code and added variables

--- SourceControlCheck/Program.cs
+++ SourceControlCheck/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, GitHub!");
+
+/// <summary>
+/// This program will be calculating the number of pizza's prices and display the total
+/// </summary>
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        // The price per pizza
+        double priceOfAPizza = 8.99;
+        // The grand total
+        double total = 0.00;
+        // User's input of number of pizzas
+        string userInput = "blank";
+        // Key for the while loop
+        bool key = true;
+
+        Console.WriteLine("Hello, GitHub!");
+
+
+    }
+}
